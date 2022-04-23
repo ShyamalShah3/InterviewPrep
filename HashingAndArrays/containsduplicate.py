003abbfd2a698eddd2b2a2010617f74430275aa6
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2022-04-23 11:14:09 -0400

    Update containsduplicate.py
    Added Problem Constraints

--- HashingAndArrays/containsduplicate.py
+++ HashingAndArrays/containsduplicate.py
@@ -1,6 +1,10 @@
 def containsDuplicate(self, nums: List[int]) -> bool:
     """
     Checks to see if a List of integers contains a duplicate
+    
+    Constraints:
+        1 <= nums.length <= 100000
+        -1000000000 <= nums[i] <= 1000000000
 
     We use a hash set to add in the elements of nums
         O(n) to add all the elements into the hash set
