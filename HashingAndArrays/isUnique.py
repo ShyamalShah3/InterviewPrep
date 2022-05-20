8ff9aeff38cc0bae7095978ffed1221a50343022
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2022-05-20 16:51:06 -0400

    Update isUnique.py
    Updated comments

--- HashingAndArrays/isUnique.py
+++ HashingAndArrays/isUnique.py
@@ -8,15 +8,15 @@ MAX_CHAR = 128 # for typical ASCII string length - change for extended ASCII
 # to solve this problem would be by sorting the input string and then iterating through the sorted string: O(nlog(n)).
 # Alternatively, the brute force approach would take O(n^2) time.
 def is_unique(s: str):
-    assert len(s) >= 0
-    if len(s) > MAX_CHAR: return False
-    if len(s) < 2: return True
-    hash_dict = {}
+    assert len(s) >= 0 # sanity check
+    if len(s) > MAX_CHAR: return False # there must be duplicate character.
+    if len(s) < 2: return True # there cannot be a duplicate character if the length is 1.
+    hash_dict = {} 
     for char in s:
-        if char in hash_dict:
+        if char in hash_dict: # if the character is already in the dictionary, it must be a duplicate. This check takes O(1) time.
             return False
         else:
-            hash_dict[char] = 1
-    return True
+            hash_dict[char] = 1 # adding character to hash map along with a dummy value of 1.
+    return True # all characters in the string must be unique
 
 
