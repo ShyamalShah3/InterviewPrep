71f6f6141e247eba0c44893bd6130bc3f139851a
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2022-06-13 21:08:34 -0400

    Update deleteMiddle.py

--- LinkedLists/deleteMiddle.py
+++ LinkedLists/deleteMiddle.py
@@ -3,14 +3,13 @@ from singlylinkedlist import LinkedList, Node
 
 def delete_middle_node(n: Node) -> None:
     """
-    Delete the middle node of a singly linked list. This code runs in O(n) time
+    Delete the middle node of a singly linked list. This code runs in O(1) time
     and has a space complexity of O(1).
     :param n: The node to delete from the singly linked list
     :return: None.
     """
-    itr = n  # iterator
-    while itr.next.next:  # while we are not at the node previous to the last node
-        itr.data = itr.next.data  # copy data from next node to current node
-        itr = itr.next
-    itr.data = itr.next.data  # copy data from last node to current node
-    itr.next = None  # delete the last node
+    if not n.next:
+        raise AssertionError("Node is not a middle node")
+    n.data = n.next.data # copy value from next node to current node
+    n.next = n.next.next # delete next node
+    
